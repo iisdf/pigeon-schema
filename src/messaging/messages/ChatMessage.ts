9f20c6649bf08d3c84e7283d84e0bf8bbf9edd87
LazyTechwork <lazytechwork@gmail.com>
2020-07-22 22:35:14 +0300

    Fixed ChatMessage.ts

--- src/messaging/messages/ChatMessage.ts
+++ src/messaging/messages/ChatMessage.ts
@@ -1,7 +1,9 @@
 import { Message } from "./Message";
 import { Attachment } from "../../attachments/Attachment";
+import { MessageType } from "../../fundamentals/message/MessageType";
 
 export interface ChatMessage extends Message {
   text?: string;
-  attachments: Attachment[];
+  attachments?: Attachment[];
+  type: MessageType.MESSAGE;
 }
